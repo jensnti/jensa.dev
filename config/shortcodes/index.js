7a310bc85273b34d065c35decb9f1511be63bde1
= <=>
2023-09-20 10:44:35 +0200

    wip: css > picture

--- config/shortcodes/index.js
+++ config/shortcodes/index.js
@@ -2,47 +2,45 @@ const fs = require('fs');
 const Image = require('@11ty/eleventy-img');
 
 const getSvgContent = (file, classList) => {
-    let relativeFilePath = `./src/_includes/assets/icons/${file}`;
-    let data = fs.readFileSync(relativeFilePath, (err, contents) => {
-        if (err) return err;
-        return contents;
-    });
-    if (classList) {
-        return (
-            data.toString('utf8').slice(0, 4) +
-            ` class="${classList}" ` +
-            data.toString('utf8').slice(4)
-        );
-    } else {
-        return data.toString('utf8');
-    }
+  let relativeFilePath = `./src/_includes/assets/icons/${file}`;
+  let data = fs.readFileSync(relativeFilePath, (err, contents) => {
+    if (err) return err;
+    return contents;
+  });
+  if (classList) {
+    return (
+      data.toString('utf8').slice(0, 4) +
+      ` class="${classList}" ` +
+      data.toString('utf8').slice(4)
+    );
+  } else {
+    return data.toString('utf8');
+  }
 };
 
 const year = () => {
-    return `${new Date().getFullYear()}`;
+  return `${new Date().getFullYear()}`;
 };
 
 const imageShortcode = async (src, alt, sizes = '100vw') => {
-    const metadata = await Image(src, {
-        widths: [300, 600, 900],
-        outputDir: './public/img/',
-    });
+  const metadata = await Image(src, {
+    widths: [300, 600, 900],
+    outputDir: './public/img/',
+  });
 
-    const imageAttributes = {
-        alt,
-        sizes,
-        loading: 'lazy',
-        decoding: 'async',
-    };
+  const imageAttributes = {
+    alt,
+    sizes,
+    loading: 'lazy',
+    decoding: 'async',
+  };
 
-    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-    return Image.generateHTML(metadata, imageAttributes, {
-        whitespaceMode: 'inline',
-    });
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
 };
 
 const youtube = (id) => {
-    return `<div class="youtube-embed">
+  return `<div class="youtube-embed">
     <iframe 
         src="https://www.youtube-nocookie.com/embed/${id}"
         title="YouTube video player"
@@ -53,8 +51,8 @@ const youtube = (id) => {
 };
 
 module.exports = {
-    youtube,
-    getSvgContent,
-    year,
-    imageShortcode,
+  youtube,
+  getSvgContent,
+  year,
+  imageShortcode,
 };
