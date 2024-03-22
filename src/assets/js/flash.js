42fa8ac66015b5a32bb1c8cffdf4ca48ebd02aa5
Jens Andreasson <jensandreasson77@gmail.com>
2024-03-22 13:45:58 +0100

    Refactor flash.js and main.js

--- src/assets/js/flash.js
+++ src/assets/js/flash.js
@@ -1,24 +1,24 @@
 const flash = (consent) => {
-    if (!consent) {
-        const flash = document.querySelector('#flash-message');
-        flash.classList.toggle('invisible');
-        flash.setAttribute('role', 'alertdialog');
-        flash.setAttribute('aria-labelledby', 'flash-message--content');
-        const inner = flash.querySelector('.flash__inner');
-        const message = inner.querySelector('.flash__message');
-        const button = document.createElement('button');
-        button.classList.add('btn', 'flash__button');
-        button.textContent = `OK`;
-        const p = document.createElement('p');
-        p.id = 'flash-message--content';
-        p.textContent = `Den här webbplatsen sparar information i din webbläsare.`;
-        message.appendChild(p);
-        inner.appendChild(button);
-        button.addEventListener('click', () => {
-            localStorage.setItem('consent', 'true');
-            flash.classList.toggle('invisible');
-        });
-    }
+  if (!consent) {
+    const flash = document.querySelector("#flash-message");
+    flash.classList.toggle("invisible");
+    flash.setAttribute("role", "alertdialog");
+    flash.setAttribute("aria-labelledby", "flash-message--content");
+    const inner = flash.querySelector(".flash__inner");
+    const message = inner.querySelector(".flash__message");
+    const button = document.createElement("button");
+    button.classList.add("btn", "flash__button");
+    button.textContent = `OK`;
+    const p = document.createElement("p");
+    p.id = "flash-message--content";
+    p.textContent = `Den här webbplatsen sparar information i din webbläsare.`;
+    message.appendChild(p);
+    inner.appendChild(button);
+    button.addEventListener("click", () => {
+      localStorage.setItem("consent", "true");
+      flash.classList.toggle("invisible");
+    });
+  }
 };
 
 module.exports = flash;
