1529967ca8872fbd2a282d55c75e0ab7e4e78459
jensadev <jensandreasson77@gmail.com>
2024-10-09 12:16:55 +0200

    Refactor language translations to include dark/light mode toggle and update contact form labels for multi-language support

--- src/_data/languages.js
+++ src/_data/languages.js
@@ -10,10 +10,16 @@ export default {
     human: "If you are a human, ignore this field",
     name: "Name",
     email: "Email",
+    email_reply: "(optional, only if you want a reply)",
     message: "Message",
     send: "Send",
     contact: "Contact",
-    dark_light: "Toggle between dark/light mode"
+    dark_light: "Toggle between dark/light mode",
+    footer: {
+      created_with: "Site created with",
+      by_me: "by Jens Andreasson",
+      published: "Published on",
+    }
   },
   sv: {
     dir: "ltr",
@@ -26,9 +32,15 @@ export default {
     human: "Om du är en människa så lämna detta fält tomt",
     name: "Namn",
     email: "E-post",
+    email_reply: "(valfritt, endast om du vill ha ett svar)",
     message: "Meddelande",
     send: "Skicka",
     contact: "Kontakt",
-    dark_light: "Växla mellan mörkt/ljust läge"
+    dark_light: "Växla mellan mörkt/ljust läge",
+    footer: {
+      created_with: "Sidan är skapad med",
+      by_me: "av Jens Andreasson",
+      published: "Publicerad på",
+    }
   },
 }
